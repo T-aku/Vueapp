68c49c3fa5ee9fba676b8ff73faf8b9f607b47a7
Ubuntu <ubuntu@ip-172-31-43-180.ap-northeast-1.compute.internal>
2020-04-01 12:28:41 +0000

    Create search botton and call api

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,37 +1,3 @@
-/* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
-// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
-// to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
-
-// import Vue from 'vue'
-// import App from '../app.vue'
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     render: h => h(App)
-//   }).$mount()
-//   document.body.appendChild(app.$el)
-
-//   console.log(app)
-// })
-
-
-// The above code uses Vue without the compiler, which means you cannot
-// use Vue to target elements in your existing html templates. You would
-// need to always use single file components.
-// To be able to target elements in your existing html/erb templates,
-// comment out the above code and uncomment the below
-// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
-// Then add this markup to your html template:
-//
-// <div id='hello'>
-//   {{message}}
-//   <app></app>
-// </div>
-
-
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 import axios from 'axios'
@@ -40,40 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#hello',
     data: {
-      message: ""
+      messages: [],
+      search: '',
+      allstores: ''
     },
-    mounted: function() {
-      axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(function(response) {
-        this.message = response.data.bpi.USD.rate
-      }.bind(this))
+    methods: {
+      onclick: function() {
+        axios
+        .get('https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=3854ab4b6c3a3a8d2a4c72c8f35951cc&freeword='+this.search)
+        .then(function(response) {
+        this.messages = response.data.rest;
+        this.allstores = '全　' + response.data.total_hit_count + '件';
+        }.bind(this))
+      }
     }
   })
 })
-//
-//
-//
-// If the project is using turbolinks, install 'vue-turbolinks':
-//
-// yarn add vue-turbolinks
-//
-// Then uncomment the code block below:
-//
-// import TurbolinksAdapter from 'vue-turbolinks'
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// Vue.use(TurbolinksAdapter)
-//
-// document.addEventListener('turbolinks:load', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: () => {
-//       return {
-//         message: "Can you say hello?"
-//       }
-//     },
-//     components: { App }
-//   })
-// })
