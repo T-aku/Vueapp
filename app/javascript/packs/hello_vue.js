8c51fd20f9aa4e16a8a1f36fbadd35089a0683c7
Ubuntu <ubuntu@ip-172-31-43-180.ap-northeast-1.compute.internal>
2020-04-03 21:08:09 +0000

    Hide APIkey

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     methods: {
       onclick: function() {
         axios
-        .get('https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=3854ab4b6c3a3a8d2a4c72c8f35951cc&freeword='+this.search)
+        .get('https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=APIKey&freeword='+this.search)
         .then(function(response) {
         this.messages = response.data.rest;
         this.allstores = '全　' + response.data.total_hit_count + '件';
